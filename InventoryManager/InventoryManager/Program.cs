773bef5cecd8c0b1eec57db68fba18378c1ba010
Nikita Luksha <nikita.luksha@softensity.com>
2022-04-20 06:40:52 +0300

    Init data

--- InventoryManager/InventoryManager/Program.cs
+++ InventoryManager/InventoryManager/Program.cs
@@ -1,5 +1,6 @@
 ﻿using InventoryManager;
 using InventoryManager.DbLibrary;
+using InventoryManager.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -7,10 +8,36 @@ IConfigurationRoot configuration;
 DbContextOptionsBuilder<InventoryDbContext> optionsBuilder;
 
 BuildOptions();
+EnsureItems();
 
 void BuildOptions()
 {
     configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
     optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
     optionsBuilder.UseSqlServer(configuration.GetConnectionString("InventoryManager"));
+}
+
+void EnsureItems()
+{
+    EnsureItem("Batman Begins");
+    EnsureItem("Inception");
+    EnsureItem("Remender the Titans");
+    EnsureItem("Star Wars");
+    EnsureItem("Top Gun");
+}
+
+void EnsureItem(string name)
+{
+    using (var db = new InventoryDbContext(optionsBuilder.Options))
+    {
+        var existingItem = db.Items.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+
+        if (existingItem == null)
+        {
+            var item = new Item { Name = name };
+
+            db.Items.Add(item);
+            db.SaveChanges();
+        }
+    }
 }

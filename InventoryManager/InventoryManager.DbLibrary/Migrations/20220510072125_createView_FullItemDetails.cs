5128fc3eb29a7d97c09735b99d3813f2c4d234a2
Mikita Luksha <nikita.luksha@softensity.com>
2022-05-10 10:45:34 +0300

    Add View

---
+++ InventoryManager/InventoryManager.DbLibrary/Migrations/20220510072125_createView_FullItemDetails.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManager.DbLibrary.Migrations.Scripts;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManager.DbLibrary.Migrations
+{
+    public partial class createView_FullItemDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.SqlResource("InventoryManager.DbLibrary.Migrations.Scripts.Views.FullItemDetails.FullItemDetails.v0.sql");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP FUNCTION IF EXISTS dbo.vwFullItemDetails");
+        }
+    }
+}

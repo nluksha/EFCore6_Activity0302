985236056af6ef8878221924ebfa38a82c48973a
Nikita Luksha <nikita.luksha@softensity.com>
2022-05-30 08:10:46 +0300

    Add Tests

---
+++ InventoryManager/InventoryManager.UnitTests/InventoryManagerUnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InventoryManager.UnitTests
+{
+    [TestClass]
+    public class InventoryManagerUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

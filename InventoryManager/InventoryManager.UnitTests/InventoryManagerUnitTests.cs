9051fe9b946609da14620e5efd04a41146f3f271
Nikita Luksha <nikita.luksha@softensity.com>
2022-05-30 21:26:24 +0300

    Refactor Tests

--- InventoryManager/InventoryManager.UnitTests/InventoryManagerUnitTests.cs
+++ InventoryManager/InventoryManager.UnitTests/InventoryManagerUnitTests.cs
@@ -7,12 +7,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using Shouldly;
 
 namespace InventoryManager.UnitTests
 {
     [TestClass]
     public class InventoryManagerUnitTests
     {
+        private const string TITLE_NEWHOPE = "Star Wars 4";
+        private const string TITLE_EMPIRE = "Star Wars 5";
+        private const string TITLE_RETURN = "Star Wars 6";
+        private const string DESC_NEWHOPE = "Luke's Friends";
+        private const string DESC_EMPIRE = "Luke's Dad";
+        private const string DESC_RETURN = "Luke's Sister";
+
+
         private static MapperConfiguration _mapperConfig;
         private static IMapper _mapper;
         private static IServiceProvider _serviceProvider;
@@ -22,38 +31,49 @@ namespace InventoryManager.UnitTests
 
         public TestContext TestContext { get; set; }
 
+
+
         [TestInitialize]
         public void InitializeTests()
         {
-            _itemsRepo = new Mock<IItemsRepo>();
+            InitializeItemsRepoMock();
+            _itemsService = new ItemsService(_itemsRepo.Object, _mapper); ;
+        }
 
-            var items = new List<Item>
+        private void InitializeItemsRepoMock()
+        {
+            _itemsRepo = new Mock<IItemsRepo>();
+            var items = GetItemsTestData();
+
+            _itemsRepo.Setup(x => x.GetItems()).Returns(items);
+        }
+
+        private List<Item> GetItemsTestData()
+        {
+            return new List<Item>
             {
                 new Item
                 {
                     Id = 1,
-                    Name = "Star Wars 4",
-                    Description = "Luke's Friends",
+                    Name = TITLE_NEWHOPE,
+                    Description = DESC_NEWHOPE,
                     CategoryId = 2
                 },
                 new Item
                 {
                     Id = 2,
-                    Name = "Star Wars 5",
-                    Description = "Luke's Dad",
+                    Name = TITLE_EMPIRE,
+                    Description = DESC_EMPIRE,
                     CategoryId = 2
                 },
                 new Item
                 {
                     Id = 3,
-                    Name = "Star wars 6",
-                    Description = "Luke's Sister",
+                    Name = TITLE_RETURN,
+                    Description = DESC_RETURN,
                     CategoryId = 2
                 }
             };
-
-            _itemsRepo.Setup(x => x.GetItems()).Returns(items);
-            _itemsService = new ItemsService(_itemsRepo.Object, _mapper); ;
         }
 
         [ClassInitialize]
@@ -76,8 +96,17 @@ namespace InventoryManager.UnitTests
         {
             var res = _itemsService.GetItems();
 
-            Assert.IsNotNull(res);
-            Assert.IsTrue(res.Count > 0);
+            res.ShouldNotBeNull();
+            res.Count.ShouldBe(3);
+            var expected = GetItemsTestData();
+
+            var item1 = res[0];
+            item1.Name.ShouldBe(TITLE_NEWHOPE);
+            item1.Description.ShouldBe(DESC_NEWHOPE);
+
+            var item2 = res[1];
+            item2.Name.ShouldBe(expected[1].Name);
+            item2.Description.ShouldBe(expected[1].Description);
         }
     }
 }

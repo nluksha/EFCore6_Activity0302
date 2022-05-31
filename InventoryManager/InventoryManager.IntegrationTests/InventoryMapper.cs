468772951ddac9f7f3c569c8d9694ac2fa33a76e
Nikita Luksha <nikita.luksha@softensity.com>
2022-05-31 10:30:18 +0300

    Add Integration tests

---
+++ InventoryManager/InventoryManager.IntegrationTests/InventoryMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using InventoryManager.Models;
+using InventoryManager.Models.DTOs;
+
+namespace InventoryManager.IntegrationTests
+{
+    public class InventoryMapper: Profile
+    {
+        public InventoryMapper()
+        {
+            CreateMaps();
+        }
+
+        private void CreateMaps()
+        {
+            CreateMap<Item, ItemDto>().ReverseMap();
+
+            CreateMap<Item, CreateOrUpdateItemDto>()
+                .ReverseMap()
+                .ForMember(x => x.Category, opt => opt.Ignore());
+
+            CreateMap<Category, CategoryDto>()
+                .ForMember(x => x.Category, opt => opt.MapFrom(y => y.Name))
+                .ReverseMap()
+                .ForMember(y => y.Name, opt => opt.MapFrom(x => x.Category));
+
+            CreateMap<CategoryDetail, CategoryDetailDto>()
+                .ForMember(x => x.Color, opt => opt.MapFrom(y => y.ColorName))
+                .ForMember(x => x.Value, opt => opt.MapFrom(y => y.ColorValue))
+                .ReverseMap()
+                .ForMember(y => y.ColorValue, opt => opt.MapFrom(x => x.Value))
+                .ForMember(y => y.ColorName, opt => opt.MapFrom(x => x.Color));
+        }
+    }
+}
